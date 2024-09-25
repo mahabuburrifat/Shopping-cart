2b532082de8cb3a263551527c23c4a8839c50d96
mahabuburrifat <mahabubur06528@gmail.com>
2024-09-25 11:56:16 +0600

    update JS & CSS

--- cart.js
+++ cart.js
@@ -1,65 +1,82 @@
-const product = [
+const showSidebar = document.getElementById('show-sidebar');
+const openSidebar = document.getElementById('open-sidebar');
+const closeSidebar = document.getElementById('close-sidebar');
+
+openSidebar.addEventListener('click', () => {
+    showSidebar.classList.add('show');
+})
+
+closeSidebar.addEventListener('click', () => {
+    showSidebar.classList.remove('show');
+})
+
+
+
+const  product = [
     {
         id: 0,
         image: './img/makeup.jpeg',
-        title: 'Z Lipstic',
-        price: 3,
+        title: 'Z Lipstick',
+        price:  5,
     },
     {
         id: 1,
         image: './img/makeup-2.jpeg',
-        title: 'Z Lipstic with box',
-        price: 5,
+        title: 'Z Lipstick with packet',
+        price:  8,
     },
     {
         id: 2,
         image: './img/makeup.jpeg',
-        title: 'Z Lipstic with box',
-        price: 3,
+        title: 'Z Lipstick',
+        price:  5,
     },
     {
         id: 3,
         image: './img/makeup-2.jpeg',
-        title: 'Z Lipstic with box',
-        price: 5,
+        title: 'Z Lipstick with packet',
+        price:  8,
     },
     {
         id: 4,
         image: './img/makeup.jpeg',
-        title: 'Z Lipstic with box',
-        price: 3,
+        title: 'Z Lipstick with packet',
+        price:  18,
     },
     {
         id: 5,
         image: './img/makeup-2.jpeg',
-        title: 'Z Lipstic with box',
-        price: 5,
+        title: 'Z Lipstick with packet',
+        price:  80,
     },
 ]
+
+ 
 
 // -------------------------------------------------\\
         //---------- show display ------------\\
 //---------------------------------------------------//\\
+    
 
-const catagories = [...new Set(product.map((item)=> {
+ const categories = [...new Set(product.map((item)=> {
     return item;
-}))]
+ }))]
     let i = 0;
-    document.getElementById('root') .innerHTML = catagories.map((item) => {
-        var {image, title, price} = item;
-        return(
-            `<div class='box'>
-                <div class='img-box'>
-                    <img class='images' src=${image}>
-                </div>
-                <div class='bottom'>
-                    <p>${title}</p>
-                    <h2>$ ${price}.00</h2>`+
-                    "<button onclick='addtocart("+ (i++) +")'>Add to Cart</button>"+
-                `</div>
-            </div>`
-        )
-    }).join('');
+     document.getElementById('root') .innerHTML = categories.map((item) => {
+         var {image, title, price} = item;
+         return(
+              `<div class='box'>
+                 <div class='img-box'>
+                     <img class='images' src=${image}>
+                 </div>
+                 <div class='bottom'>
+                     <p>${title}</p>
+                     <h2>$ ${price}.00</h2>` +
+                     "<button class='btn' onclick='addtocart("+ (i++) +")'>Add to Cart</button>" +
+                 `</div>
+              </div>`       
+         )
+     }).join('');
 
 
 
@@ -67,58 +84,57 @@ const catagories = [...new Set(product.map((item)=> {
         //--------- function call --------------\\
 //----------------------------------------------------------------\\
 
+     var cart = [];
 
-    var cart = [];
+     function addtocart(a){
+         cart.push({...categories[a]});
+         displayCart();
+     };
 
-    function addtocart(a){
-        cart.push({...catagories[a]});
-        displayCart();
-    }
+     function delElement(a){
+         cart.splice(a, 1);
+         displayCart();
+     }
 
-    function delElement(a){
-        cart.splice(a, 1);
-        displayCart();
-    }
-
-
+     
 //------------------------------------------------------------------\\
         //----------------show sidebar---------------------\\
 //--------------------------------------------------------------------\\
 
 
-    function displayCart(a){
-        let j = 0; total = 0;
+     function displayCart(a){
+         let j = 0; total = 0;
 
-        document.getElementById('count') .innerHTML = cart.length;
+         document.getElementById('count') .innerHTML = cart.length;
 
-        if(cart.length == 0){
-            document.getElementById('cartItem').innerHTML = 'Your Cart is empty';
-            document.getElementById('total').innerHTML = '$ '+ 0 +'.00';
-        }
-        else{
-            document.getElementById('cartItem').innerHTML = cart.map((items)=> {
-                var{image, title, price} = items;
-            
+
+         if(cart.length == 0){
+             document.getElementById('cartItem') .innerHTML = 'Your Cart is empty';
+
             ////---------------- calculate price------------\\
+             document.getElementById('total').innerHTML = '$ '+ 0 +'.00';
 
-                total = total + price;
-                document.getElementById('total').innerHTML = '$ '+ total +'.00';
+         }
+         else{
+             document.getElementById('cartItem') .innerHTML = cart.map((items) => {
+                 var {image, title, price} = items;
 
-            //\\---------------- end caclculate price--------//
+                ////---------------- calculate price------------\\
 
-                return(
-                    `<div class='cart-item'>
-                        <div class='row-img'>
-                            <img class='rowimg'>src=${image}></img>
-                        </div>
-                        <p style='font-size:12px;'>${title}</p>
-                        <h2 style='font-size:15px;'>$ ${price}.00</h2>`+
-                        "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
-                );
-            }).join('');
-        }
-    }
+                 total = total + price;
+                 document.getElementById('total').innerHTML = '$ '+ total +'.00';
 
+                //\\---------------- end caclculate price--------//
 
-
-    
+                 return(
+                     `<div class='cart-item'>
+                         <div class='row-img'>
+                             <img class='rowimg' src=${image}>
+                         </div>
+                         <p style='font-size:15px;'>${title}</p>
+                         <h2 style='font-size:15px;'>$ ${price}.00</h2>`+
+                         "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+                 )
+             }).join('');
+         }
+     }
